293694a2e714cdccc35a8b3a832ec6a97ad25742
Drukhary <RKrisanov-0@yandex.ru>
2021-07-29 11:03:53 +0300

    testing

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+let counter = 0;
+router.get('/posts', function(req, res, next) {
+  res.json({a:1,b:2,counter:counter++});
+  // res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
